1d30b6208b44cb5db1e8f9cb6bffcc21c7b80df5
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-09-25 23:51:02 +0530

    services rows

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -25,6 +25,30 @@ const ServicesData = [
       "Lorem ipsum dolor sit amet ipsum dolor sit ametipsum dolor sit amet ipsum dolor sit amet",
     aosDelay: "500",
   },
+  {
+    id: 4,
+    img: Img2,
+    name: "Espresso",
+    description:
+      "Lorem ipsum dolor sit ipsum dolor sit dolor sit ipsum dolor sit amet.",
+    aosDelay: "100",
+  },
+  {
+    id: 5,
+    img: Img2,
+    name: "Americano",
+    description:
+      "Lorem ipsum dolor sit amet ipsum dolor sit ametipsum dolor sit amet ipsum dolor sit amet",
+    aosDelay: "300",
+  },
+  {
+    id: 6,
+    img: Img2,
+    name: "Cappuccino",
+    description:
+      "Lorem ipsum dolor sit amet ipsum dolor sit ametipsum dolor sit amet ipsum dolor sit amet",
+    aosDelay: "500",
+  },
 ];
 
 const Services = () => {
@@ -33,28 +57,28 @@ const Services = () => {
       <span id="services"></span>
       <div className="py-10 bg-white"> {/* Added gray background here */}
         <div className="container">
-          {/* Heading section  */}
+          {/* Heading section */}
           <div className="mb-20 text-center">
             <h1 className="text-4xl font-bold text-black font-cursive"> {/* Changed text color to black */}
               Best Coffee For You
             </h1>
           </div>
 
-          {/* Services Card section  */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-14 md:gap-5 place-items-center">
+          {/* Services Card section */}
+          <div className="grid grid-cols-1 gap-20 sm:grid-cols-2 md:grid-cols-3 md:gap-20 place-items-center"> {/* Increased gap between rows */}
             {ServicesData.map((service) => (
               <div
                 key={service.id}
                 data-aos="fade-up"
                 data-aos-delay={service.aosDelay}
-                className="rounded-2xl bg-black text-yellow-600  hover:bg-yellow-600 hover:text-black relative shadow-xl duration-high group max-w-[300px]"
+                className="rounded-2xl bg-black text-yellow-600 hover:bg-yellow-600 hover:text-black relative shadow-xl duration-high group max-w-[300px]"
               >
                 <div className="h-[122px]">
                   <img
                     src="/cofe2.png"
                     alt=""
                     className="max-w-[200px] block mx-auto transform -translate-y-14
-                  group-hover:scale-105 group-hover:rotate-6 duration-300"
+                    group-hover:scale-105 group-hover:rotate-6 duration-300"
                   />
                 </div>
                 <div className="p-4 text-center">
