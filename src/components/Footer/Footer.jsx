356b8034f8dc93d91ca7cd204e007bcb14e46847
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-09-27 22:09:16 +0530

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -21,7 +21,7 @@ const FooterLinks = [
 
 const Footer = () => {
   return (
-    <div className="-mt-10 text-white bg-gray-800"> {/* Background color and moved up */}
+    <div className="-mt-10 text-white bg-amber-900"> {/* Changed background to brown */}
       <div className="bg-black/40 min-h-[250px]"> {/* Reduced minimum height */}
         <div className="container grid pt-4 pb-10 md:grid-cols-3"> {/* Reduced padding */}
           {/* Company details */}
