6746f7c8e149e9ad020394bd7636d01cd08f49fb
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-09-29 19:38:09 +0530

    order

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,15 +1,15 @@
-
+import beansImage from "../../assets/beans.jpg";
 
 const Order = () => {
   return (
     <div
       className="relative h-[300px] flex items-center justify-center bg-cover bg-center"
       style={{
-        backgroundImage: `url('/assets/beans.jpg')`, 
+        backgroundImage: `url(${beansImage})`, // Use the imported image here
       }}
     >
       <div className="absolute inset-0 bg-black opacity-60"></div> {/* Overlay */}
-      
+
       <div className="relative px-4 text-center text-white">
         <h1 className="text-4xl font-bold">
           A MORNING WITHOUT <span className="text-[#d2691e]">COFFEE</span> LIKE A SLEEP
