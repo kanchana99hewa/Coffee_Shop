aeebb9b64a2a958b0abdd02383c507a004b1e335
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-10-16 22:33:18 +0530

    .

--- src/components/Pages/Cart.jsx
+++ src/components/Pages/Cart.jsx
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
 
 const Cart = ({ cart, setCart, handleCheckout }) => {
+  // Function to remove item from cart by productId
   const removeFromCart = (productId) => {
     setCart(cart.filter((item) => item.id !== productId));
   };
 
+  // Function to increase quantity of an item in the cart by productId
   const increaseQuantity = (productId) => {
     setCart(
       cart.map((item) =>
@@ -14,7 +16,7 @@ const Cart = ({ cart, setCart, handleCheckout }) => {
   };
 
   return (
-    <div className="p-4 text-black bg-white rounded shadow-lg top-16 right-5 w-80">
+    <div className="p-4 text-black bg-white rounded shadow-lg w-80">
       <h2 className="mb-4 text-xl font-bold">Cart</h2>
       {cart.length === 0 ? (
         <p>Your cart is empty.</p>
@@ -34,7 +36,7 @@ const Cart = ({ cart, setCart, handleCheckout }) => {
                 </button>
                 <button
                   onClick={() => removeFromCart(item.id)}
-                  className="px-2 py-1 bg-red-500 rounded"
+                  className="px-2 py-1 text-white bg-red-500 rounded"
                 >
                   Remove
                 </button>
@@ -43,9 +45,10 @@ const Cart = ({ cart, setCart, handleCheckout }) => {
           ))}
         </ul>
       )}
+
       <button
         onClick={handleCheckout}
-        className="px-4 py-2 mt-4 text-white bg-green-500 rounded"
+        className="w-full px-4 py-2 mt-4 text-white bg-green-500 rounded"
       >
         Checkout
       </button>
